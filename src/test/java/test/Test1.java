c7a28383f3e20a5202e0300c9afa03d32471a488
SunDRise <sunrise19399@gmail.com>
2022-04-10 12:17:38 +0530

    frst commit

---
+++ src/test/java/test/Test1.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=2;
+		int b=4;
+		
+		int c=a*b;
+		
+		System.out.println(c);
+	}
+
+}
